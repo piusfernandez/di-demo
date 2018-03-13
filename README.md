78982dd55f6b8ceb93325bc1c2f0950383abbe07
Pius N Fernandez <piusfernandez@hotmail.com>
2018-03-13 07:23:45 -0400

    Dependency Injection demo

---
+++ README.md
@@ -0,0 +1,2 @@
+Learn Dependency Injection
+Get a handle on ApplicationContext and fetch the hello bean
---
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.didemo.controller.MyController;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+	 ApplicationContext ctx =	SpringApplication.run(DiDemoApplication.class, args);
+	 MyController myController = (MyController)ctx.getBean("myController");
+	 myController.hello();
+	}
+}
