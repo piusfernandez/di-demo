9796495dd839c0591271f82a6737f9401414c64b
Pius N Fernandez <piusfernandez@hotmail.com>
2018-03-13 18:04:48 -0400

    @Profile default

--- src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -6,13 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-// default is only active when there are not other active profile in application.properties
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
 
 	@Override
 	public String sayGreeting() {
-		return "Hello - I am qualified by @Primary";
+		return "Primarer Grudienst";
 	}
 
 }
